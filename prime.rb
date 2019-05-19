03dde499800ae545d6f7b8b9e844ec32ed5e70b9
Trish Ogen <patriciaogen@gmail.com>
2019-05-19 16:47:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  else
+    range=(2..int-1).to_a
+    if range.any? {|element| int % element == 0}
+      return false
+    else
+      return true
+    end
+  end
+end
